a6577fac4d277d743e6a7c004c103598704ce7c2
Allan Pilarca <allan@wizmediateam.com>
2011-05-24 11:35:16 +0800

    Initial commit

---
+++ administrator/components/com_album/views/html.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+* 
+*/
+class ComAlbumViewHtml extends ComDefaultViewHtml
+{
+    
+    public function __construct(KConfig $config)
+    {
+        $config->views = array(
+            'albums' => JText::_('Albums'),
+            'photos' => JText::_('Photos')
+        );
+
+        parent::__construct($config);
+    }
+}
